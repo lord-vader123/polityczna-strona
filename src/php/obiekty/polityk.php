face849e64f1c4fc06889764d7d79826df2ad089
Szymon P <czarnyspajdi@gmail.com>
2024-07-01 17:37:28 +0200

    dodano klasę polityk

---
+++ src/php/obiekty/polityk.php
@@ -0,0 +1,14 @@
+<?php 
+
+class polityk {
+    // parametry
+    private string $imie, $nazwisko, $sciezka_do_obrazu;
+    private ?int $id, $id_partii, $id_komitetu;
+    private bool $jest_poslem;
+
+
+    public function __construct() {
+
+    }
+    
+}
