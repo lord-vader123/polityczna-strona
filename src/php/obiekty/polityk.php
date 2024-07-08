260c2d37c4718f59354bc9a8b2737a47d3f0eadb
Szymon P <czarnyspajdi@gmail.com>
2024-07-08 18:01:08 +0200

    dodano konstruktor

--- src/php/obiekty/polityk.php
+++ src/php/obiekty/polityk.php
@@ -6,22 +6,17 @@ class polityk {
     private ?int $id, $id_partii, $id_komitetu;
     private bool $jest_poslem;
 
-
-    public function __construct($imie, $nazwisko, $sciezka_do_obrazu, $id, $id_partii, $id_komitetu, $jest_poslem) {
-        if ($id === NULL) {
-            // Dodaj posła do bazy danych
-            return;
+    public function __construct($imie, $nazwisko, $sciezka_do_obrazu, $id_partii = NULL, $id_komitetu = NULL, $jest_poslem, mysqli $conn, $id = NULL) {
+        if ($id !=== NULL) {
+            // pobierz dane z bazy danych
         } else {
-            // Ustaw istniejące dane
-            $this->id = $id;
-            $this->id_partii = $id_partii;
-            $this->id_komitetu = $id_komitetu;
-            $this->jest_poslem = $jest_poslem;
+            this -> $imie = $imie;
+            this -> $nazwisko = $nazwisko;
+            this -> $sciezka_do_obrazu = $sciezka_do_obrazu;
+            this -> $id_partii = $id_partii;
+            this -> $id_komitetu = $id_komitetu;
+            this -> $jest_poslem = $jest_poslem; 
         }
-        
-        $this->imie = $imie;
-        $this->nazwisko = $nazwisko;
-        $this->sciezka_do_obrazu = $sciezka_do_obrazu;
     }
 }
 
