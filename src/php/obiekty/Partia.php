11ce0af692bae29671fd6d44ef40ab1aba27f630
Szymon P <czarnyspajdi@gmail.com>
2024-07-09 13:30:00 +0200

    nadpisano metode przypiszDane

--- src/php/obiekty/Partia.php
+++ src/php/obiekty/Partia.php
@@ -13,11 +13,19 @@ class Partia extends ElementBazy {
             $this->skrot_nazwy = $skrot_nazwy;
             $this->sciezka_do_obrazu = $sciezka_do_obrazu;
         }
-
         
+        protected function przypiszDane(array $dane) {
+            $this->id = $dane['id_partii'];
+            $this->nazwa_partii = $dane['nazwa_partii'];
+            $this->skrot_nazwy = $dane['skrot_nazwy'];
+            $this->sciezka_do_obrazu = $dane['sciezka_do_obrazu'];
+        }
+        
+        protected function pobierzTabele() {
+            return "Partie";
+        }
         
     }
-
 
 }
 
