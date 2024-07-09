4d1b5d253be3a0eff2ef8997392b769ea568b2fe
Szymon P <czarnyspajdi@gmail.com>
2024-07-09 13:25:00 +0200

    dodano konstruktor

--- src/php/obiekty/Partia.php
+++ src/php/obiekty/Partia.php
@@ -2,6 +2,18 @@
 
 class Partia extends ElementBazy {
 
+    private ?int $id;
+    private ?string $nazwa_partii, $skrot_nazwy,  $sciezka_do_obrazu;
+    
+    public function __construct($id = NULL, $conn = NULL, $nazwa_partii, $skrot_nazwy, $sciezka_do_obrazu) {
+        if ($id !== NULL && $conn !== NULL) {
+            parent::__construct($id, $conn);
+        }
+        
+        
+        
+    }
+
 
 }
 
