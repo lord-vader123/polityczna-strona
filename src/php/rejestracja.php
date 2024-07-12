a73b4505eee99035d21c7ad67021e7634db5f701
Szymon P <czarnyspajdi@gmail.com>
2024-07-12 18:27:38 +0200

    dołączono style

--- src/php/rejestracja.php
+++ src/php/rejestracja.php
@@ -7,6 +7,7 @@ include_once '/php/login.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rejestracja</title>
+    <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
     
