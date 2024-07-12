a52e142b59022a9d9cba1f77da115c78632c30be
Szymon P <czarnyspajdi@gmail.com>
2024-07-12 18:52:12 +0200

    dodano formularz

--- src/php/rejestracja.php
+++ src/php/rejestracja.php
@@ -11,9 +11,23 @@ include_once '/php/login.php';
 </head>
 <body>
 
-<header>
-    <a href="/index.php">Powrót</a>
-</header>
+    <header>
+        <a href="/index.php">Powrót</a>
+    </header>
+
+    <div class="content">
+        <form action="<?php echo htmlspecialchars($_SERVER['php_self']); ?>" method="post">
+            <label for="imie">Imię</label>
+            <input type="text" name="imie">
+            <label for="nazwisko">Nazwisko</label>
+            <input type="text" name="nazwisko">
+            <label for="login">Login/Mail</label>
+            <input type="email" name="login">
+            <label for="haslo">Hasło</label>
+            <input type="password" name="haslo">
+            <input type="submit" value="Zarejestruj się">
+        </form>
+    </div>
     
 </body>
 </html>
