35ccc75c68b9c063a57aefe1f2c1f3ae5b9b1c0d
Szymon P <czarnyspajdi@gmail.com>
2024-07-14 13:07:51 +0200

    Przeniesiono ustawianie parametrów z konstuktora na gettery i settery

--- src/php/obiekty/Polityk.php
+++ src/php/obiekty/Polityk.php
@@ -9,18 +9,10 @@ class Polityk extends ElementBazy{
     private bool $jest_poslem;
     protected ?int $id;
 
-    public function __construct($imie, $nazwisko, $sciezka_do_obrazu, $id_partii = NULL, $id_komitetu = NULL, $jest_poslem, mysqli $conn = NULL, $id = NULL) {
+    public function __construct(?int $id = NULL, ?mysqli $conn) {
         if ($id !== NULL && $conn !== NULL) {
             parent::__construct($id, $conn);
-        } else {
-            $this->id = $id;
-            $this->imie = $imie;            
-            $this->nazwisko = $nazwisko;
-            $this->id_partii = $id_partii;
-            $this->id_komitetu = $id_komitetu;
-            $this->jest_poslem = $jest_poslem;
-        }
-
+        }     
     }
     
     protected function przypiszDane(array $dane): void {
@@ -35,6 +27,60 @@ class Polityk extends ElementBazy{
 
     protected function pobierzTabele(): string {
         return "Politycy";
+    }
+
+    // Gettery
+    public function getImie(): string {
+        return $this->imie;
+    }
+
+    public function getNazwisko(): string {
+        return $this->nazwisko;
+    }
+
+    public function getSciezkaDoObrazu(): string {
+        return $this->sciezka_do_obrazu;
+    }
+
+    public function getIdPartii(): ?int {
+        return $this->id_partii;
+    }
+
+    public function getIdKomitetu(): ?int {
+        return $this->id_komitetu;
+    }
+
+    public function getJestPoslem(): bool {
+        return $this->jest_poslem;
+    }
+
+    public function getId(): ?int {
+        return $this->id;
+    }
+
+    // Settery
+    public function setImie(string $imie): void {
+        $this->imie = $imie;
+    }
+
+    public function setNazwisko(string $nazwisko): void {
+        $this->nazwisko = $nazwisko;
+    }
+
+    public function setSciezkaDoObrazu(string $sciezka_do_obrazu): void {
+        $this->sciezka_do_obrazu = $sciezka_do_obrazu;
+    }
+
+    public function setIdPartii(?int $id_partii): void {
+        $this->id_partii = $id_partii;
+    }
+
+    public function setIdKomitetu(?int $id_komitetu): void {
+        $this->id_komitetu = $id_komitetu;
+    }
+
+    public function setJestPoslem(bool $jest_poslem): void {
+        $this->jest_poslem = $jest_poslem;
     }
 }
 
