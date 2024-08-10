069c7b2834891da704f9a171fec98ee996a8fa44
Szymon P <szymon_jozef@proton.me>
2024-08-10 18:56:24 +0200

    Dodano klasę Party

---
+++ src/php/obiekty/Party.php
@@ -0,0 +1,13 @@
+<?php
+
+class Party extends MySqlObject {
+    
+    public function __construct(mysqli|null $conn, int|null $id) {
+        parent::__construct($conn, $id);
+
+    }
+
+    public function getTable(): string {
+        return "Parties";
+    }
+}
