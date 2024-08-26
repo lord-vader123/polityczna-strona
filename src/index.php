d1bc4ef8940cd90ebe23cedc8945c638111869d6
Jakub <kubak998112@gmail.com>
2024-08-26 19:01:31 +0200

    Poprawka

--- src/index.php
+++ src/index.php
@@ -16,12 +16,42 @@
     <h1> Polityczna Strona </h1>
     </div>
     <div class="menu">
-      <a href="index.php">Strona główna</a> | <a href="about:blank">Lista partii</a> | <a href="about:blank">Lista politykow</a> | <a href="about:blank">Dodaj Polityka</a> | <a href="about:blank">Dodaj partie</a>
+      <div class="navbar">
+        <a href="index.php">Strona Główna</a>
+        <a href="about:blank">Aktualności</a>
+
+        <div class="dropdown">
+        <button class="dropbtn">Listy<i class="fa fa-caret-down"></i></button>
+        <div class="dropdown-content">
+          <a href="about:blank">Partie</a>
+          <a href="about:blank">Politycy</a>
+        </div>
+      </div> 
+
+      <div class="dropdown">
+        <button class="dropbtn">Narzędzia<i class="fa fa-caret-down"></i></button>
+        <div class="dropdown-content">
+          <a href="about:blank">Dodaj partie</a>
+          <a href="about:blank">Dodaj polityka</a>
+        </div>
+      </div> 
+    </div>
+    <!--Wymaga popraw (zrobie to kiedyś) -->
+
+
   </div>
+
   <div class="login">
-  <a href="/php/login.php">Zaloguj się</a> <br>
-  <a href="/php/registration.php">Zarejestruj się</a>
+      <div class="dropdown">
+      <img style="width: 100px; height: 100px;" src="/assets/img/trzymaczmiejsca.png">
+        <div class="dropdown-content">
+          <a href="/php/login.php">Zaloguj się</a>
+          <a href="/php/registration.php">Zarejestruj się</a>
+        </div>
+
+
   </div>
+</div>
   </header>
 
   <div class="content">
@@ -30,6 +60,7 @@
   <p>Aktualna ilość polityków: </p>
 
   </div>
+  
 
 </body>
 
