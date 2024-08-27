aa8cfbabaf4843acde91b697cdbbcd28fe78ef72
Szymon P <szymon_jozef@proton.me>
2024-08-27 17:08:29 +0200

    Wydzielono nagłówek do pliku /php/header.php

--- src/index.php
+++ src/index.php
@@ -11,35 +11,7 @@
 
 <body>
 
-  <header>
-    <div class="title">
-    <h1> Polityczna Strona </h1>
-    </div>
-    <div class="menu">
-      <div class="navbar">
-        <a href="index.php">Strona Główna</a>
-        <a href="about:blank">Aktualności</a>
-
-        <div class="dropdown">
-        <button class="dropbtn">Listy<i class="fa fa-caret-down"></i></button>
-        <div class="dropdown-content">
-          <a href="about:blank">Partie</a>
-          <a href="about:blank">Politycy</a>
-        </div>
-      </div> 
-
-      <div class="dropdown">
-        <button class="dropbtn">Narzędzia<i class="fa fa-caret-down"></i></button>
-        <div class="dropdown-content">
-          <a href="about:blank">Dodaj partie</a>
-          <a href="about:blank">Dodaj polityka</a>
-        </div>
-      </div> 
-    </div>
-    <!--Wymaga popraw (zrobie to kiedyś) -->
-
-
-  </div>
+  <?php include_once __DIR__ . '/php/header.php'; ?>
 
   <div class="login">
       <div class="dropdown">
---
+++ src/php/header.php
@@ -0,0 +1,29 @@
+  <header>
+    <div class="title">
+    <h1> Polityczna Strona </h1>
+    </div>
+    <div class="menu">
+      <div class="navbar">
+        <a href="index.php">Strona Główna</a>
+        <a href="about:blank">Aktualności</a>
+
+        <div class="dropdown">
+        <button class="dropbtn">Listy<i class="fa fa-caret-down"></i></button>
+        <div class="dropdown-content">
+          <a href="about:blank">Partie</a>
+          <a href="about:blank">Politycy</a>
+        </div>
+      </div> 
+
+      <div class="dropdown">
+        <button class="dropbtn">Narzędzia<i class="fa fa-caret-down"></i></button>
+        <div class="dropdown-content">
+          <a href="about:blank">Dodaj partie</a>
+          <a href="about:blank">Dodaj polityka</a>
+        </div>
+      </div> 
+    </div>
+    <!--Wymaga popraw (zrobie to kiedyś) -->
+
+
+  </div>
