779314a0cf8be55c24e667515eed5ce937bf9f4f
Szymon P <szymon_jozef@proton.me>
2024-08-29 12:21:02 +0200

    Dodano formularz

--- src/php/settings.php
+++ src/php/settings.php
@@ -17,6 +17,25 @@ $userData = $user->getDataArray();
 <body>
 
 <?php include_once __DIR__ . '/html-snippets/header.html'; ?>
+
+<div class="content">
+
+<form action="<? htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post">
+
+<p>Wypełnij dane które pragniesz zmienić</p>
+<label for="name">Imię</label>
+<input type="text" id="name">
+<label for="surname">Nazwisko</label>
+<input type="text" id="surname">
+<label for="email">Adres e-mail</label>
+<input type="email" id="email">
+<label for="passphrase">Hasło</label>
+<input type="password" id="passphrase">
+<button type="submit">Zatwierdź</button>
+
+</form>
+
+</div>
     
 </body>
 </html>
