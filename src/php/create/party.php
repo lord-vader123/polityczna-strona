bcfb39815a1b2e0e0eafb9113fadf37a1031f3bc
Szymon P <szymon_jozef@proton.me>
2024-08-30 20:26:10 +0200

    Dodano formularz

--- src/php/create/party.php
+++ src/php/create/party.php
@@ -20,7 +20,13 @@ include_once __DIR__ . '/../objects/Party.php';
     <div class="content">
 
     <form action="<?php htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post">
-    
+    <label for="long_name">Pełna nazwa</label>        
+    <input type="text" name="long_name" id="long_name">
+    <label for="short_name">Skrót nazwy</label>
+    <input type="text" name="short_name" id="short_name" >
+    <label for="logo">Logo partii</label>
+    <input type="file" name="logo" id="logo">
+
         
     </form>
 
