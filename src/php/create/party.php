c87ec055bea33a9f221432fb566c9bbc5fa83a17
Szymon P <szymon_jozef@proton.me>
2024-08-30 20:00:56 +0200

    Poprawiono importy

--- src/php/create/party.php
+++ src/php/create/party.php
@@ -1,19 +1,30 @@
 <?php
-include_once __DIR__ . '../scripts/login-mysql.php'; 
-include_once __DIR__ . '../objects/Party.php';
+include_once __DIR__ . '/../scripts/login-mysql.php';
+include_once __DIR__ . '/../objects/Party.php';
 ?>
-
 <!DOCTYPE html>
 <html lang="pl">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php include_once __DIR__ . "../html-snippets/icons.html"; ?>
+    <link rel="stylesheet" href="/css/style.css">
     <title>Stwórz partię</title>
 </head>
+
 <body>
 
-<?php include_once __DIR__ . '../html-snippets/header-logged.html'; ?>
+    <?php include_once __DIR__ . '/../html-snippets/header-logged.html'; ?>
+
+    <div class="content">
+
+    <form action="<?php htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post">
     
+        
+    </form>
+
+    </div>
 </body>
+
 </html>
