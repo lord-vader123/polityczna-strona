a84fa9b0e89bbca67daa5b58f168c11c879044e2
Szymon P <szymon_jozef@proton.me>
2024-08-30 19:39:13 +0200

    Dodano strukturę strony

---
+++ src/php/create/party.php
@@ -0,0 +1,19 @@
+<?php
+include_once __DIR__ . '../scripts/login-mysql.php'; 
+include_once __DIR__ . '../objects/Party.php';
+?>
+
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include_once __DIR__ . "../html-snippets/icons.html"; ?>
+    <title>Stwórz partię</title>
+</head>
+<body>
+
+<?php include_once __DIR__ . '../html-snippets/header-logged.html'; ?>
+    
+</body>
+</html>
