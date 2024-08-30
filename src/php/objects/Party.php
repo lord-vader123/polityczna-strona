66795d452d00c3f122612bd1844c6f72e72f90c0
Szymon P <szymon_jozef@proton.me>
2024-08-30 19:45:43 +0200

    Zaimplementowano metodę setData()

--- src/php/objects/Party.php
+++ src/php/objects/Party.php
@@ -12,4 +12,19 @@ class Party extends MySqlObject {
     public function getTable(): string {
         return "parties";
     }
+    
+    public function setData(array $data): bool {
+        if (is_array($data) && count($data) == 3) {
+            $dbData = [
+                'full_name' => $data['full_name'],
+                'short_name' => $data['short_name'],
+                'logo' => $data['logo'],
+            ];
+            $this->setDataArray($dbData);
+            return true;
+        } else {
+            throw new Exception('Wrong argument provided');
+        }
+
+    }
 }
