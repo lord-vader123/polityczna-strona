1ca347bf0aa96ce595db0afdb7ef9d8a6197ed50
Szymon P <szymon_jozef@proton.me>
2024-08-31 16:43:24 +0200

    Dodano limit rozmiaru pliku

--- src/php/create/party.php
+++ src/php/create/party.php
@@ -35,6 +35,12 @@ include __DIR__ . '/../objects/Party.php';
     ini_set('display_errors', 1);
     error_reporting(E_ALL);
     if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['logo']) && $_FILES['logo']['error'] == UPLOAD_ERR_OK) {
+        
+        if ($_FILES['logo']['size'] > 1073741824) {
+            echo "Plik jest za duży!";
+            exit();
+        }
+        
         $imageHandler = new ImageHandler('party', $_FILES['logo']); 
         $imageHandler->saveFile();
         
