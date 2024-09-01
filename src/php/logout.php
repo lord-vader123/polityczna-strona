baf14853f200146f8c9095a5442e53fddfc58950
Szymon P <szymon_jozef@proton.me>
2024-09-01 13:51:57 +0200

    Dostosowano do nowych ciasteczek

--- src/php/logout.php
+++ src/php/logout.php
@@ -3,7 +3,8 @@
 session_unset();
 session_destroy();
 
-setcookie('id', '', time() -3600,'/');
+setcookie('login', '', time() -3600,'/');
+setcookie('passphrase', '', time() -3600,'/');
 
 header('Location: /index.php');
 exit();
