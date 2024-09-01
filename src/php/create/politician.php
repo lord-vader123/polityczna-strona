2f9e6c3a56e57f18b96c7d4600b9feb258f04ed8
Szymon P <szymon_jozef@proton.me>
2024-09-01 19:04:53 +0200

    Utworzono szkielet strony

---
+++ src/php/create/politician.php
@@ -0,0 +1,25 @@
+<?
+include_once __DIR__ . '/../objects/Politician.php';
+include_once __DIR__ . '/../scripts/login-mysql.php';
+?>
+
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include_once __DIR__ . '/../html-snippets/icons.html'; ?>
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Dodaj polityka</title>
+</head>
+<body>
+
+<?php include_once __DIR__ . '/../html-snippets/header-logged.html'; ?> 
+
+<div class="content">
+
+
+</div>
+    
+</body>
+</html>
