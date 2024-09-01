1450c8886cfba83e4810aa201ecd84b6af66fc1b
Szymon P <szymon_jozef@proton.me>
2024-09-01 20:50:03 +0200

    Dodano zapis danych do bazy danych

--- src/php/create/politician.php
+++ src/php/create/politician.php
@@ -10,6 +10,7 @@ include_once __DIR__ . '/../scripts/login-mysql.php';
 
 <!DOCTYPE html>
 <html lang="pl">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -17,58 +18,84 @@ include_once __DIR__ . '/../scripts/login-mysql.php';
     <link rel="stylesheet" href="/css/style.css">
     <title>Dodaj polityka</title>
 </head>
+
 <body>
 
-<?php include_once __DIR__ . '/../html-snippets/header-logged.html'; ?> 
+    <?php include_once __DIR__ . '/../html-snippets/header-logged.html'; ?>
 
-<div class="content">
+    <div class="content">
 
-    <form action="<?php htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" enctype="multipart/form-data">
-        <label for="name">Imię</label>
-        <input type="text" name="name" id="name">
+        <form action="<?php htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" enctype="multipart/form-data">
+            <label for="name">Imię</label>
+            <input type="text" name="name" id="name">
 
-        <label for="surname">Nazwisko</label>
-        <input type="text" name="surname" id="surname">
+            <label for="surname">Nazwisko</label>
+            <input type="text" name="surname" id="surname">
 
-        <label for="party">Partia</label>
-        <input list="party" name="party" >
+            <label for="party">Partia</label>
+            <input list="party" name="party">
 
 
-        <label for="committee">Komitet wyborczy</label>
-        <input list="committee" name="committee">
+            <label for="committee">Komitet wyborczy</label>
+            <input list="committee" name="committee">
 
-        <label for="portrait">Portret polityka</label>
-        <input type="file" name="portrait" id="portrait">
+            <label for="portrait">Portret polityka</label>
+            <input type="file" name="portrait" id="portrait">
 
-        <button type="submit">Zatwierdź</button>
+            <button type="submit">Zatwierdź</button>
 
-        <datalist id="party">
-            <?php
-            $parties = new Table($conn, "party");
-            $data = $parties->getTableArray();
-            foreach ($data as $row) {
-                echo '<option value="' . $row['id'] . '">' . $row['full_name']. '</option>';
+            <datalist id="party">
+                <?php
+                $parties = new Table($conn, "party");
+                $data = $parties->getTableArray();
+                foreach ($data as $row) {
+                    echo '<option value="' . $row['id'] . '">' . $row['full_name'] . '</option>';
+                }
+                unset($parties);
+                ?>
+            </datalist>
+
+
+            <datalist id="committee">
+                <?php
+                $committees = new Table($conn, "committee");
+                $data = $committees->getTableArray();
+                foreach ($data as $row) {
+                    echo '<option value="' . $row['id'] . '">' . $row['name'] . '</option>';
+                }
+                unset($committees);
+                ?>
+            </datalist>
+        </form>
+        
+        <?php
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['portrait']) && $_FILES['portrait']['error'] == UPLOAD_ERR_OK) {
+
+        
+            $imageHandler = new ImageHandler('politician', $_FILES['portrait']);
+            $imageHandler->saveFile();
+            $imagePath = $imageHandler->getFinalPath();
+            
+            $data = [
+                'name' => $conn->real_escape_string($_POST['name']),
+                'sursurname' => $conn->real_escape_string($_POST['surname']),
+                'party' => $conn->real_escape_string($_POST['party']),
+                'committee' => $conn->real_escape_string($_POST['committee']),
+                'portrait' => $imagePath,
+            ];
+            
+            try {
+                $politician = new Politician($conn, null);
+                $politician->setData($data);
+                $politician->sendToDb();
+            } catch (Exception $e) {
+                echo "An error has occurred";
             }
-            unset($parties);
-            ?>
-        </datalist>
+        }
+        ?>
 
+    </div>
 
-        <datalist id="committee">            
-            <?php
-            $committees = new Table($conn, "committee");
-            $data = $committees->getTableArray();
-            foreach ($data as $row) {
-                echo '<option value="' . $row['id'] . '">' . $row['name']. '</option>';
-            }
-            unset($committees);
-            ?>
-        </datalist>
-
-
-    </form>
-
-</div>
-    
 </body>
+
 </html>
