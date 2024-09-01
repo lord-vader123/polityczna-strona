3fec782a2e79166bae97793ff6eb457628bca369
Szymon P <szymon_jozef@proton.me>
2024-09-01 19:33:58 +0200

    Utworzono szkielet formularza

--- src/php/create/politician.php
+++ src/php/create/politician.php
@@ -1,5 +1,6 @@
 <?
 include_once __DIR__ . '/../objects/Politician.php';
+include_once __DIR__ . '/../objects/ImageHandler.php';
 include_once __DIR__ . '/../scripts/login-mysql.php';
 ?>
 
@@ -18,6 +19,36 @@ include_once __DIR__ . '/../scripts/login-mysql.php';
 
 <div class="content">
 
+    <form action="<?php htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" enctype="multipart/form-data">
+        <label for="name">Imię</label>
+        <input type="text" name="name" id="name">
+
+        <label for="surname">Nazwisko</label>
+        <input type="text" name="surname" id="surname">
+
+        <label for="party">Partia</label>
+        <input list="party" name="party" >
+
+
+        <label for="committee">Komitet wyborczy</label>
+        <input list="committee" name="committee">
+
+        <label for="portrait">Portret polityka</label>
+        <input type="file" name="portrait" id="portrait">
+
+        <button type="submit">Zatwierdź</button>
+
+        <datalist id="party">
+            <option value=""></option>
+            
+        </datalist>
+
+        <datalist id="committee">            
+            <option value="">        
+        </datalist>
+
+
+    </form>
 
 </div>
     
