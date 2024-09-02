8e07598cd49bcae4df7969482fb859cbe5e23662
Szymon P <szymon_jozef@proton.me>
2024-09-02 19:10:15 +0200

    Dodano obsługę danych w bazie danych

--- src/php/create/committee.php
+++ src/php/create/committee.php
@@ -1,6 +1,9 @@
 <?php
 include_once __DIR__ . '/../scripts/login-mysql.php';
 include_once __DIR__ . '/../objects/Committee.php';
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 ?>
 
 <!DOCTYPE html>
@@ -18,14 +21,30 @@ include_once __DIR__ . '/../objects/Committee.php';
 
 <div class="content">
 
-    <form action="<?php htmlspecialchars($_SERVER['PHP_SELF'] == 'POST') ?>" method="post">
+    <form action="<?php htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post">
         <label for="name">Nazwa komitetu</label>
         <input type="text" name="name" id="name">
         <button type="submit">Zatwierdź</button>
     </form>
 
     <div id="error">
+    <?php
+    if($_SERVER['REQUEST_METHOD'] == 'POST' ) {
+        $data = [
+            'name' => $conn->real_escape_string($_POST['name']),
+        ];
+
+        try {
+            $committee = new Committee($conn, null);
+            $committee->setData($data);
+            $committee->sendToDb();
+            echo "Komitet ". $data['name'] . " dodany!";
+        } catch (Exception $e) {
+            echo 'Błąd: '. $e->getMessage() .'<br>';
+        }
+    }
     
+    ?>
         
     </div>
 </div>
