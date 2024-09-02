c995b3eda6b2297c34d359d9bb2d97f61adf7301
Szymon P <szymon_jozef@proton.me>
2024-09-02 18:47:38 +0200

    Dodano szkic strony

---
+++ src/php/create/committee.php
@@ -0,0 +1,20 @@
+<?php
+include_once __DIR__ . '/../scripts/login-mysql.php';
+include_once __DIR__ . '/../objects/Committee.php';
+?>
+
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include_once __DIR__ . '/../html-snippets/icons.html'; ?>
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Utwórz komitet</title>
+</head>
+<body>
+
+<?php include_once __DIR__ . '/../html-snippets/header-logged.html'; ?>
+    
+</body>
+</html>
