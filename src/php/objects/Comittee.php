266950679b12f511ed726c1f340b2a260d1e67fb
Szymon P <szymon_jozef@proton.me>
2024-09-02 18:58:59 +0200

    Zaimplementowano metody

--- src/php/objects/Comittee.php
+++ src/php/objects/Comittee.php
@@ -12,4 +12,33 @@ class Committee extends MySqlObject {
     public function getTable(): string {
         return "Committees";
     }
+    
+    public function setData(array $data): bool {
+        if (is_array($data) && count($data) == 1) {
+            $data = [
+                'name' => $data['name'],
+            ];
+            $this->setDataArray($data);
+            return true;
+        } else {
+            throw new Exception('Incorrect argument provided');
+        }
+    }
+    
+    public function sendToDb(): bool {
+        $data = $this->getDataArray();
+        $stmt = $this->conn->prepare('INSERT INTO comittee (name) VALUES (?)');
+        if (!$stmt) {
+            throw new Exception('Error preparing statement');
+        }
+        
+        $stmt->bind_param('s', $data['name']);
+        
+        if (!$stmt->execute()) {
+            throw new Exception('Something broke while executing query');
+        }
+        
+        $stmt->close();
+        return true;
+    }
 }
